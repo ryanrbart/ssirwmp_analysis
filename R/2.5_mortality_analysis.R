e6f8609fd229cb851a558377d7a901cc52e1af04
ryanrbart <ryanrbart@eri.ucsb.edu>
2018-05-17 15:52:38 -0700

    Updated mortality figures and started future mortality analysis

--- R/2.5_mortality_analysis.R
+++ R/2.5_mortality_analysis.R
@@ -40,7 +40,7 @@ pet_hamon_month <- function(x, daylight_hours){
 
 
 # ---------------------------------------------------------------------
-# 
+# Process mortality data
 
 # Precip 2015
 precip_2015 <- calc(prism_precip_stack, sum)
@@ -52,51 +52,109 @@ pet_2015 <- calc(pet_2015_stack, sum)
 names(pet_2015) <- "pet_2015"
 
 # PET 2085 (RCP4.5)
-pet_2085_stack <- pet_hamon_month(x=(prism_temp_stack + 3.25), daylight_hours=daylight_hours)
-pet_2085 <- calc(pet_2085_stack, sum)
-names(pet_2085) <- "pet_2085"
+pet_2085_45_stack <- pet_hamon_month(x=(prism_temp_stack + 3.09), daylight_hours=daylight_hours) # 3.09 from mean annual projected temp increase for min and max
+pet_2085_45 <- calc(pet_2085_45_stack, sum)
+names(pet_2085_45) <- "pet_2085_45"
+
+# PET 2085 (RCP8.5)
+pet_2085_85_stack <- pet_hamon_month(x=(prism_temp_stack + 5.09), daylight_hours=daylight_hours) # 5.09 from mean annual projected temp increase for min and max
+pet_2085_85 <- calc(pet_2085_85_stack, sum)
+names(pet_2085_85) <- "pet_2085_85"
 
 # CWD 2015
 cwd_2015 <- precip_2015 - pet_2015
 names(cwd_2015) <- "cwd_2015"
 
-# CWD 2085
-cwd_2085 <- precip_2015 - pet_2085
-names(cwd_2085) <- "cwd_2085"
+# CWD 2085 (RCP4.5)
+# (Ideally we would use MACA projections, but they won't reflect severe drought conditions unless I search for driest years)
+cwd_2085_45 <- precip_2015 - pet_2085_45
+names(cwd_2085_45) <- "cwd_2085_45"
+
+# CWD 2085 (RCP8.5)
+cwd_2085_85 <- precip_2015 - pet_2085_85
+names(cwd_2085_85) <- "cwd_2085_85"
 
 
-#plot(cwd)
-#plot(y_15[[4]])
+happy_tib <- as_tibble(values(stack(y_15, cwd_2015, cwd_2085_45, cwd_2085_85)))
 
-happy_tib <- as_tibble(values(stack(y_15, precip_2015, pet_2015, pet_2085, cwd_2015, cwd_2085)))
+happy_tib <- happy_tib %>%
+  #gather("future_period", "cwd", c(cwd_2015,cwd_2085_45, cwd_2085_85)) %>% 
+  dplyr::filter(mort.tph>=0)
 
-happy_tib <- happy_tib %>% 
-  dplyr::filter(mort.tph<5)
 
-# -----
-# Young 'equivalent' plot
+# ---------------------------------------------------------------------
+# Figures
 
-#test <- function(x)(x*.063+34)
-test <- function(x)(0.00004*((x+900)^2))
+# Add a line that encloses the mortality region
+#mort_line <- function(x)(x*.063+34)
+mort_line <- function(x)(0.00004*((x+900)^2))
 
-ggplot(data = happy_tib,aes(x=cwd_2015,y=live.bah)) +
+# This plot is similar to Young 2017 Fig 3.
+happy_tib %>% 
+  dplyr::filter(future_period=="cwd_2015") %>% 
+  ggplot(data = .,aes(x=cwd,y=live.bah)) +
   geom_point(aes(color=mort.tph)) +
-  scale_color_gradient(low="yellow", high="black") +
-  xlim(-850,250) +
-  #ylim(0,60) +
-  stat_function(fun=test)
-
-ggplot(data = happy_tib,aes(x=cwd_2085,y=live.bah)) +
-  geom_point(aes(color=mort.tph)) +
-  scale_color_gradient(low="yellow", high="black") +
-  xlim(-850,250) +
-  ylim(0,60) +
-  stat_function(fun=test)
+  scale_color_gradient(low="navajowhite1", high="black", name="Mortality\n(trees/ha)") +
+  stat_function(fun=mort_line) +
+  labs(title="Forest Mortality", x="Climatic Water Deficit",y="Live Basal Area per Hectare", size=0.5) +
+  theme_bw(base_size =12)
+ggsave("output/mortality_single.jpg", width = 6, height = 5)
 
 
+ggplot(data = happy_tib,aes(x=cwd,y=live.bah)) +
+  geom_point(color="gray") +
+  scale_color_gradient(low="navajowhite1", high="black", name="Mortality\nTrees/Hect") +
+  stat_function(fun=mort_line) +
+  labs(title="Forest Mortality", x="Climatic Water Deficit",y="Live Basal Area per Hectare", size=0.5) +
+  theme_bw(base_size =12) +
+  facet_grid(.~future_period, labeller = as_labeller(c(
+    `cwd_2015` = "Drought 2015",
+    `cwd_2085_45` = "Drought 2085 (RCP4.5)",
+    `cwd_2085_85` = "Drought 2085 (RCP8.5)")))
+ggsave("output/mortality_multiple.jpg", width = 7, height = 5)
 
-sum(happy_tib$live.bah > test(happy_tib$cwd_2015), na.rm = TRUE)
-sum(happy_tib$live.bah > test(happy_tib$cwd_2085), na.rm = TRUE)
+
+# ---------------------------------------------------------------------
+# Rates of mortality above the line
+
+happy_tib <- as_tibble(values(stack(y_15, cwd_2015, cwd_2085_45, cwd_2085_85)))
+
+# Total number of pixels
+mort_all <- happy_tib %>%
+  dplyr::filter(mort.tph>=0, live.bah>=10)
+nrow(mort_all)
+
+# Total number of pixels with mort.tph > 10
+mort_10 <- happy_tib %>%
+  dplyr::filter(mort.tph>=5, live.bah>10)
+nrow(mort_10)
+
+# Historical scenario
+# Pixels above mort line
+a <- sum(mort_all$live.bah > mort_line(mort_all$cwd_2015), na.rm = TRUE)
+b <- sum(mort_10$live.bah > mort_line(mort_10$cwd_2015), na.rm = TRUE)
+mort_ratio <- b/a
+mort_percent <- b/nrow(mort_all)
+print(paste("Percent region affected:", round(mort_percent,3)))
+
+# RCP4.5 Scenario
+a <- sum(mort_all$live.bah > mort_line(mort_all$cwd_2085_45), na.rm = TRUE)
+b <- sum(mort_10$live.bah > mort_line(mort_10$cwd_2085_45), na.rm = TRUE)
+b_mod <- a*mort_ratio
+mort_percent <- b_mod/nrow(mort_all)
+print(paste("Percent region affected:", round(mort_percent,3)))
+
+# RCP8.5 Scenario
+a <- sum(mort_all$live.bah > mort_line(mort_all$cwd_2085_85), na.rm = TRUE)
+b <- sum(mort_10$live.bah > mort_line(mort_10$cwd_2085_85), na.rm = TRUE)
+b_mod <- a*mort_ratio
+mort_percent <- b_mod/nrow(mort_all)
+print(paste("Percent region affected:", round(mort_percent,3)))
+
+
+
+# ---------------------------------------------------------------------
+# Map of mortality
 
 
 
@@ -108,7 +166,7 @@ sum(happy_tib$live.bah > test(happy_tib$cwd_2085), na.rm = TRUE)
 # ---------------------------------------------------------------------
 # ---------------------------------------------------------------------
 # ---------------------------------------------------------------------
-# Time-series (processing)
+# USFS mortality data
 
 head(ss_mort_2017)
 

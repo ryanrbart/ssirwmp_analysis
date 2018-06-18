f8eeeff507f9eb63e09669a8250292944fbba558
ryanrbart <ryanrbart@eri.ucsb.edu>
2018-06-17 23:12:36 -0700

    Refined VIC outputs

--- R/2.6_vic.R
+++ R/2.6_vic.R
@@ -202,19 +202,23 @@ etqswe_season_by_fp <- etqswe_season %>%
 ggplot() + 
   geom_boxplot(data=etqswe_annual_by_fp, aes(future_period, Q, fill=rcp),
                color="black", outlier.shape = NA) +
-  labs(title = "Projected Annual Streamflow", x = "Period", y = "Annual Streamflow (mm)") +
+  labs(title = "Projected Mean Annual Streamflow for Kings River", x = "Period", y = "Mean Annual Streamflow (mm)") +
   geom_hline(data=etqswe_annual_by_fp, aes(yintercept = Q_hist_annual2), color="gray20") +
   scale_fill_discrete(name="RCP", labels = c("4.5","8.5")) +
+  # Conversion to acre-feet from https://gist.github.com/ryanrbart/e4d274034d2c1737d73d. Kings watershed = 4001 km2
+  scale_y_continuous(sec.axis = sec_axis(~.*4001*0.000000810713192, name = "Mean Annual Streamflow\n(millions of acre feet)")) +
   theme_bw()
-ggsave("output/Q_box_annual.jpg", width = 4, height = 3)
+ggsave("output/Q_box_annual.jpg", width = 5, height = 4)
 
 # Changes in Q
 ggplot() + 
   geom_boxplot(data=etqswe_annual_by_fp, aes(future_period, Q_annual_diff, fill=rcp),
                color="black", outlier.shape = NA) +
-  labs(title = "Projected Change in Annual Streamflow", x = "Period", y = "Change in Annual Streamflow (mm)") +
+  labs(title = "Projected Change in Mean Annual Streamflow for Kings River", x = "Period", y = "Change in Mean Annual Streamflow (mm)") +
   geom_hline(yintercept = 0) +
   scale_fill_discrete(name="RCP", labels = c("4.5","8.5")) +
+  # Conversion to acre-feet from https://gist.github.com/ryanrbart/e4d274034d2c1737d73d. Kings watershed = 4001 km2
+  scale_y_continuous(sec.axis = sec_axis(~.*4001*0.000000810713192, name = "Change in Mean Annual Streamflow\n(millions of acre feet)")) +
   theme_bw()
 ggsave("output/Q_box_annual_delta.jpg", width = 4, height = 3)
 
@@ -227,29 +231,33 @@ etqswe_annual_by_fp %>%
 # Box plot by time-periods (monthly)
 
 month_id <- c(
-  `1` = "Jan", `2` = "Feb", `3` = "Mar",`4` = "Apr",
-  `5` = "May",`6` = "Jun",`7` = "Jul",`8` = "Aug",
-  `9` = "Sep",`10` = "Oct",`11` = "Nov",`12` = "Dec"
+  `1` = "January", `2` = "February", `3` = "March",`4` = "April",
+  `5` = "May",`6` = "June",`7` = "July",`8` = "August",
+  `9` = "September",`10` = "October",`11` = "November",`12` = "December"
 )
 
 # Q
 ggplot() + 
   geom_boxplot(data=etqswe_month_by_fp, aes(future_period, Q, fill=rcp),
                color="black", outlier.shape = NA) +
-  labs(title = "Projected Monthly Streamflow", x = "Period", y = "Monthly Streamflow (mm)") +
+  labs(title = "Projected Mean Monthly Streamflow for Kings River", x = "Period", y = "Mean Monthly Streamflow (mm)") +
   geom_hline(data=etqswe_month_by_fp, aes(yintercept = Q_hist_month2), color="gray20") +
   scale_fill_discrete(name="RCP", labels = c("4.5","8.5")) +
-  facet_wrap(~month, labeller = as_labeller(month_id)) +
-  theme_bw()
-ggsave("output/Q_box_month.jpg", width = 8, height = 6)
+  # Conversion to acre-feet from https://gist.github.com/ryanrbart/e4d274034d2c1737d73d. Kings watershed = 4001 km2
+  scale_y_continuous(sec.axis = sec_axis(~.*4001*0.000000810713192, name = "Mean Monthly Streamflow (millions of acre feet)")) +
+  facet_wrap(~month, labeller = as_labeller(month_id), nrow=4) +
+  theme_bw(base_size = 13)
+ggsave("output/Q_box_month.jpg", width = 8, height = 10)
 
 # Changes in Q
 ggplot() + 
   geom_boxplot(data=etqswe_month_by_fp, aes(future_period, Q_month_diff, fill=rcp),
                color="black", outlier.shape = NA) +
-  labs(title = "Projected Change in Monthly Streamflow", x = "Period", y = "Change in Monthly Streamflow (mm)") +
+  labs(title = "Projected Change in Mean Monthly Streamflow for Kings River", x = "Period", y = "Change in Mean Monthly Streamflow (mm)") +
   geom_hline(yintercept = 0) +
   scale_fill_discrete(name="RCP", labels = c("4.5","8.5")) +
+  # Conversion to acre-feet from https://gist.github.com/ryanrbart/e4d274034d2c1737d73d. Kings watershed = 4001 km2
+  scale_y_continuous(sec.axis = sec_axis(~.*4001*0.000000810713192, name = "Change in Mean Monthly Streamflow (millions of acre feet)")) +
   facet_wrap(~month, labeller = as_labeller(month_id)) +
   theme_bw()
 ggsave("output/Q_box_month_delta.jpg", width = 8, height = 6)
@@ -272,9 +280,11 @@ season_id <- c(
 ggplot() + 
   geom_boxplot(data=etqswe_season_by_fp, aes(future_period, Q, fill=rcp),
                color="black", outlier.shape = NA) +
-  labs(title = "Projected Seasonal Streamflow", x = "Period", y = "Seasonal Streamflow (mm)") +
+  labs(title = "Projected Mean Seasonal Streamflow for Kings River", x = "Period", y = "Mean Seasonal Streamflow (mm)") +
   geom_hline(data=etqswe_season_by_fp, aes(yintercept = Q_hist_season2), color="gray20") +
   scale_fill_discrete(name="RCP", labels = c("4.5","8.5")) +
+  # Conversion to acre-feet from https://gist.github.com/ryanrbart/e4d274034d2c1737d73d. Kings watershed = 4001 km2
+  scale_y_continuous(sec.axis = sec_axis(~.*4001*0.000000810713192, name = "Mean Seasonal Streamflow\n(millions of acre feet)")) +
   facet_wrap(~season, labeller = as_labeller(season_id)) +
   theme_bw()
 ggsave("output/Q_box_season.jpg", width = 5, height = 4)
@@ -283,9 +293,11 @@ ggsave("output/Q_box_season.jpg", width = 5, height = 4)
 ggplot() + 
   geom_boxplot(data=etqswe_season_by_fp, aes(future_period, Q_season_diff, fill=rcp),
                color="black", outlier.shape = NA) +
-  labs(title = "Projected Change in Seasonal Streamflow", x = "Period", y = "Change in Seasonal Streamflow (mm)") +
+  labs(title = "Projected Change in Mean Seasonal Streamflow for Kings River", x = "Period", y = "Change in Mean Seasonal Streamflow (mm)") +
   geom_hline(yintercept = 0) +
   scale_fill_discrete(name="RCP", labels = c("4.5","8.5")) +
+  # Conversion to acre-feet from https://gist.github.com/ryanrbart/e4d274034d2c1737d73d. Kings watershed = 4001 km2
+  scale_y_continuous(sec.axis = sec_axis(~.*4001*0.000000810713192, name = "Change in Mean Seasonal Streamflow\n(millions of acre feet)")) +
   facet_wrap(~season, labeller = as_labeller(season_id)) +
   theme_bw()
 ggsave("output/Q_box_season_delta.jpg", width = 5, height = 4)
@@ -332,18 +344,19 @@ etqswe_annual_by_fp %>%
 # Box plot by time-periods (monthly ET)
 
 month_id <- c(
-  `1` = "Jan", `2` = "Feb", `3` = "Mar",`4` = "Apr",
-  `5` = "May",`6` = "Jun",`7` = "Jul",`8` = "Aug",
-  `9` = "Sep",`10` = "Oct",`11` = "Nov",`12` = "Dec"
+  `1` = "January", `2` = "February", `3` = "March",`4` = "April",
+  `5` = "May",`6` = "June",`7` = "July",`8` = "August",
+  `9` = "September",`10` = "October",`11` = "November",`12` = "December"
 )
 
 # ET
 ggplot() + 
   geom_boxplot(data=etqswe_month_by_fp, aes(future_period, ET, fill=rcp),
                color="black", outlier.shape = NA) +
-  labs(title = "Projected Monthly ET", x = "Period", y = "Monthly ET (mm)") +
+  labs(title = "Projected Mean Monthly Evapotranspiration (ET) in Kings Watershed", x = "Period", y = "Mean Monthly ET (mm)") +
   geom_hline(data=etqswe_month_by_fp, aes(yintercept = ET_hist_month2), color="gray20") +
   scale_fill_discrete(name="RCP", labels = c("4.5","8.5")) +
+  scale_y_continuous(sec.axis = sec_axis(~./25.4, name = "Mean Monthly ET (in)")) +
   facet_wrap(~month, labeller = as_labeller(month_id)) +
   theme_bw()
 ggsave("output/ET_box_month.jpg", width = 8, height = 6)
@@ -352,9 +365,10 @@ ggsave("output/ET_box_month.jpg", width = 8, height = 6)
 ggplot() + 
   geom_boxplot(data=etqswe_month_by_fp, aes(future_period, ET_month_diff, fill=rcp),
                color="black", outlier.shape = NA) +
-  labs(title = "Projected Change in Monthly ET", x = "Period", y = "Change in Monthly ET (mm)") +
+  labs(title = "Projected Change in Mean Monthly ET", x = "Period", y = "Change in Mean Monthly ET (mm)") +
   geom_hline(yintercept = 0) +
   scale_fill_discrete(name="RCP", labels = c("4.5","8.5")) +
+  scale_y_continuous(sec.axis = sec_axis(~./25.4, name = "Change in Mean Monthly ET (in)")) +
   facet_wrap(~month, labeller = as_labeller(month_id)) +
   theme_bw()
 ggsave("output/ET_box_month_delta.jpg", width = 8, height = 6)
@@ -377,12 +391,13 @@ season_id <- c(
 ggplot() + 
   geom_boxplot(data=etqswe_season_by_fp, aes(future_period, ET, fill=rcp),
                color="black", outlier.shape = NA) +
-  labs(title = "Projected Seasonal ET", x = "Period", y = "Seasonal ET (mm)") +
+  labs(title = "Projected Mean Seasonal Evapotranspiration (ET) in Kings Watershed", x = "Period", y = "Mean Seasonal ET (mm)") +
   geom_hline(data=etqswe_season_by_fp, aes(yintercept = ET_hist_season2), color="gray20") +
   scale_fill_discrete(name="RCP", labels = c("4.5","8.5")) +
+  scale_y_continuous(limits = c(0,235),sec.axis = sec_axis(~./25.4, name = "Change in Mean Seasonal ET (in)")) +
   facet_wrap(~season, labeller = as_labeller(season_id)) +
   theme_bw()
-ggsave("output/ET_box_season.jpg", width = 5, height = 4)
+ggsave("output/ET_box_season.jpg", width = 6.5, height = 5)
 
 # Changes in ET
 ggplot() + 
@@ -438,29 +453,31 @@ etqswe_annual_by_fp %>%
 # Box plot by time-periods (monthly SWE)
 
 month_id <- c(
-  `1` = "Jan", `2` = "Feb", `3` = "Mar",`4` = "Apr",
-  `5` = "May",`6` = "Jun",`7` = "Jul",`8` = "Aug",
-  `9` = "Sep",`10` = "Oct",`11` = "Nov",`12` = "Dec"
+  `1` = "January", `2` = "February", `3` = "March",`4` = "April",
+  `5` = "May",`6` = "June",`7` = "July",`8` = "August",
+  `9` = "September",`10` = "October",`11` = "November",`12` = "December"
 )
 
 # SWE
 ggplot() + 
   geom_boxplot(data=etqswe_month_by_fp, aes(future_period, SWE, fill=rcp),
                color="black", outlier.shape = NA) +
-  labs(title = "Projected Monthly Peak Snow Water Equivalent (SWE)", x = "Period", y = "Monthly Peak SWE (mm)") +
+  labs(title = "Projected Monthly Peak Snow Water Equivalent (SWE) for Kings Watershed", x = "Period", y = "Mean Monthly Peak SWE (mm)") +
   geom_hline(data=etqswe_month_by_fp, aes(yintercept = SWE_hist_month2), color="gray20") +
   scale_fill_discrete(name="RCP", labels = c("4.5","8.5")) +
-  facet_wrap(~month, labeller = as_labeller(month_id)) +
-  theme_bw()
-ggsave("output/SWE_box_month.jpg", width = 8, height = 6)
+  scale_y_continuous(sec.axis = sec_axis(~./25.4, name = "Mean Monthly Peak SWE (in)")) +
+  facet_wrap(~month, labeller = as_labeller(month_id), nrow=4) +
+  theme_bw(base_size = 13)
+ggsave("output/SWE_box_month.jpg", width = 8.1, height = 10)
 
 # Changes in SWE
 ggplot() + 
   geom_boxplot(data=etqswe_month_by_fp, aes(future_period, SWE_month_diff, fill=rcp),
                color="black", outlier.shape = NA) +
-  labs(title = "Projected Change in Monthly Peak Snow Water Equivalent (SWE)", x = "Period", y = "Change in Monthly Peak SWE (mm)") +
+  labs(title = "Projected Change in Monthly Peak Snow Water Equivalent (SWE)", x = "Period", y = "Change in Mean Monthly Peak SWE (mm)") +
   geom_hline(yintercept = 0) +
   scale_fill_discrete(name="RCP", labels = c("4.5","8.5")) +
+  scale_y_continuous(sec.axis = sec_axis(~.*25.4, name = "Change in Mean Monthly Peak SWE (in)")) +
   facet_wrap(~month, labeller = as_labeller(month_id)) +
   theme_bw()
 ggsave("output/SWE_box_month_delta.jpg", width = 8, height = 6)

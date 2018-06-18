905752455c19ddc54a343a28f0313f9abe8a74f7
ryanrbart <ryanrbart@eri.ucsb.edu>
2018-06-17 23:14:46 -0700

    Added import paths for VIC distributed annual average ET and SWE data

--- R/1_import.R
+++ R/1_import.R
@@ -435,9 +435,85 @@ etqswe_kings <- list(
 
 # ----
 # Sierra (aka WY average ET and Peak SWE - distributed)
-wy_et_canesm2_hist <- readr::read_csv("../../VIC_Modeling_SS/Sierra/CanESM2_Historical_1951_2005_WY_ET.csv")
+
+wy_et_canesm2_hist <- readr::read_csv("../../VIC_Modeling_SS/Sierra//CanESM2_Historical_1951_2005_WY_ET.csv")
+wy_et_canesm2_45 <- readr::read_csv("../../VIC_Modeling_SS/Sierra//CanESM2_RCP45_2007_2099_WY_ET.csv")
+wy_et_canesm2_85 <- readr::read_csv("../../VIC_Modeling_SS/Sierra//CanESM2_RCP85_2007_2099_WY_ET.csv")
+wy_et_ccsm4_hist <- readr::read_csv("../../VIC_Modeling_SS/Sierra//CCSM4_Historical_1951_2005_WY_ET.csv")
+wy_et_ccsm4_45 <- readr::read_csv("../../VIC_Modeling_SS/Sierra//CCSM4_RCP45_2007_2099_WY_ET.csv")
+wy_et_ccsm4_85 <- readr::read_csv("../../VIC_Modeling_SS/Sierra//CCSM4_RCP85_2007_2099_WY_ET.csv")
+wy_et_cnrm_hist <- readr::read_csv("../../VIC_Modeling_SS/Sierra//CNRM-CM5_Historical_1951_2005_WY_ET.csv")
+wy_et_cnrm_45 <- readr::read_csv("../../VIC_Modeling_SS/Sierra//CNRM-CM5_RCP45_2007_2099_WY_ET.csv")
+wy_et_cnrm_85 <- readr::read_csv("../../VIC_Modeling_SS/Sierra//CNRM-CM5_RCP85_2007_2099_WY_ET.csv")
+wy_et_hadgemcc_hist <- readr::read_csv("../../VIC_Modeling_SS/Sierra//HadGEM2-CC365_Historical_1951_2005_WY_ET.csv")
+wy_et_hadgemcc_45 <- readr::read_csv("../../VIC_Modeling_SS/Sierra//HadGEM2-CC365_RCP45_2007_2099_WY_ET.csv")
+wy_et_hadgemcc_85 <- readr::read_csv("../../VIC_Modeling_SS/Sierra//HadGEM2-CC365_RCP85_2007_2099_WY_ET.csv")
+wy_et_hadgemec_hist <- readr::read_csv("../../VIC_Modeling_SS/Sierra//HadGEM2-ES365_Historical_1951_2005_WY_ET.csv")
+wy_et_hadgemec_45 <- readr::read_csv("../../VIC_Modeling_SS/Sierra//HadGEM2-ES365_RCP45_2007_2099_WY_ET.csv")
+wy_et_hadgemec_85 <- readr::read_csv("../../VIC_Modeling_SS/Sierra//HadGEM2-ES365_RCP85_2007_2099_WY_ET.csv")
+wy_et_miroc5_hist <- readr::read_csv("../../VIC_Modeling_SS/Sierra//MIROC5_Historical_1951_2005_WY_ET.csv")
+wy_et_miroc5_45 <- readr::read_csv("../../VIC_Modeling_SS/Sierra//MIROC5_RCP45_2007_2099_WY_ET.csv")
+wy_et_miroc5_85 <- readr::read_csv("../../VIC_Modeling_SS/Sierra//MIROC5_RCP85_2007_2099_WY_ET.csv")
+
+wy_swe_canesm2_hist <- readr::read_csv("../../VIC_Modeling_SS/Sierra//CanESM2_Historical_1951_2005_WY_PEAK_SWE.csv")
+wy_swe_canesm2_45 <- readr::read_csv("../../VIC_Modeling_SS/Sierra//CanESM2_RCP45_2007_2099_WY_PEAK_SWE.csv")
+wy_swe_canesm2_85 <- readr::read_csv("../../VIC_Modeling_SS/Sierra//CanESM2_RCP85_2007_2099_WY_PEAK_SWE.csv")
+wy_swe_ccsm4_hist <- readr::read_csv("../../VIC_Modeling_SS/Sierra//CCSM4_Historical_1951_2005_WY_PEAK_SWE.csv")
+wy_swe_ccsm4_45 <- readr::read_csv("../../VIC_Modeling_SS/Sierra//CCSM4_RCP45_2007_2099_WY_PEAK_SWE.csv")
+wy_swe_ccsm4_85 <- readr::read_csv("../../VIC_Modeling_SS/Sierra//CCSM4_RCP85_2007_2099_WY_PEAK_SWE.csv")
+wy_swe_cnrm_hist <- readr::read_csv("../../VIC_Modeling_SS/Sierra//CNRM-CM5_Historical_1951_2005_WY_PEAK_SWE.csv")
+wy_swe_cnrm_45 <- readr::read_csv("../../VIC_Modeling_SS/Sierra//CNRM-CM5_RCP45_2007_2099_WY_PEAK_SWE.csv")
+wy_swe_cnrm_85 <- readr::read_csv("../../VIC_Modeling_SS/Sierra//CNRM-CM5_RCP85_2007_2099_WY_PEAK_SWE.csv")
+wy_swe_hadgemcc_hist <- readr::read_csv("../../VIC_Modeling_SS/Sierra//HadGEM2-CC365_Historical_1951_2005_WY_PEAK_SWE.csv")
+wy_swe_hadgemcc_45 <- readr::read_csv("../../VIC_Modeling_SS/Sierra//HadGEM2-CC365_RCP45_2007_2099_WY_PEAK_SWE.csv")
+wy_swe_hadgemcc_85 <- readr::read_csv("../../VIC_Modeling_SS/Sierra//HadGEM2-CC365_RCP85_2007_2099_WY_PEAK_SWE.csv")
+wy_swe_hadgemec_hist <- readr::read_csv("../../VIC_Modeling_SS/Sierra//HadGEM2-ES365_Historical_1951_2005_WY_PEAK_SWE.csv")
+wy_swe_hadgemec_45 <- readr::read_csv("../../VIC_Modeling_SS/Sierra//HadGEM2-ES365_RCP45_2007_2099_WY_PEAK_SWE.csv")
+wy_swe_hadgemec_85 <- readr::read_csv("../../VIC_Modeling_SS/Sierra//HadGEM2-ES365_RCP85_2007_2099_WY_PEAK_SWE.csv")
+wy_swe_miroc5_hist <- readr::read_csv("../../VIC_Modeling_SS/Sierra//MIROC5_Historical_1951_2005_WY_PEAK_SWE.csv")
+wy_swe_miroc5_45 <- readr::read_csv("../../VIC_Modeling_SS/Sierra//MIROC5_RCP45_2007_2099_WY_PEAK_SWE.csv")
+wy_swe_miroc5_85 <- readr::read_csv("../../VIC_Modeling_SS/Sierra//MIROC5_RCP85_2007_2099_WY_PEAK_SWE.csv")
 
 
+vic_maps <- list(
+  wy_et_canesm2_hist=wy_et_canesm2_hist,
+  wy_et_canesm2_45=wy_et_canesm2_45,
+  wy_et_canesm2_85=wy_et_canesm2_85,
+  wy_et_ccsm4_hist=wy_et_ccsm4_hist,
+  wy_et_ccsm4_45=wy_et_ccsm4_45,
+  wy_et_ccsm4_85=wy_et_ccsm4_85,
+  wy_et_cnrm_hist=wy_et_cnrm_hist,
+  wy_et_cnrm_45=wy_et_cnrm_45,
+  wy_et_cnrm_85=wy_et_cnrm_85,
+  wy_et_hadgemcc_hist=wy_et_hadgemcc_hist,
+  wy_et_hadgemcc_45=wy_et_hadgemcc_45,
+  wy_et_hadgemcc_85=wy_et_hadgemcc_85,
+  wy_et_hadgemec_hist=wy_et_hadgemec_hist,
+  wy_et_hadgemec_45=wy_et_hadgemec_45,
+  wy_et_hadgemec_85=wy_et_hadgemec_85,
+  wy_et_miroc5_hist=wy_et_miroc5_hist,
+  wy_et_miroc5_45=wy_et_miroc5_45,
+  wy_et_miroc5_85=wy_et_miroc5_85,
+  
+  wy_swe_canesm2_hist=wy_swe_canesm2_hist,
+  wy_swe_canesm2_45=wy_swe_canesm2_45,
+  wy_swe_canesm2_85=wy_swe_canesm2_85,
+  wy_swe_ccsm4_hist=wy_swe_ccsm4_hist,
+  wy_swe_ccsm4_45=wy_swe_ccsm4_45,
+  wy_swe_ccsm4_85=wy_swe_ccsm4_85,
+  wy_swe_cnrm_hist=wy_swe_cnrm_hist,
+  wy_swe_cnrm_45=wy_swe_cnrm_45,
+  wy_swe_cnrm_85=wy_swe_cnrm_85,
+  wy_swe_hadgemcc_hist=wy_swe_hadgemcc_hist,
+  wy_swe_hadgemcc_45=wy_swe_hadgemcc_45,
+  wy_swe_hadgemcc_85=wy_swe_hadgemcc_85,
+  wy_swe_hadgemec_hist=wy_swe_hadgemec_hist,
+  wy_swe_hadgemec_45=wy_swe_hadgemec_45,
+  wy_swe_hadgemec_85=wy_swe_hadgemec_85,
+  wy_swe_miroc5_hist=wy_swe_miroc5_hist,
+  wy_swe_miroc5_45=wy_swe_miroc5_45,
+  wy_swe_miroc5_85=wy_swe_miroc5_85
+)
 
 # ---------------------------------------------------------------------
 # Import DEM

4a5e8d370bdbd3fdc2f8c104cfd3a6e88a2778a4
ryanrbart <ryanrbart@eri.ucsb.edu>
2018-09-06 11:38:44 -0700

    Finalized SWE map figure

--- R/2.6_vic_maps.R
+++ R/2.6_vic_maps.R
@@ -34,6 +34,9 @@ vic_maps_tib$long <- as.double(vic_maps_tib$long)
 period_levels <- c("hist", "45", "85")
 vic_maps_tib$period <- factor(vic_maps_tib$period, levels = period_levels)
 
+
+# ----
+# ET Processing
 vic_maps_et <- vic_maps_tib %>% 
   dplyr::filter(flux_type=="et") %>% 
   #mutate(value_interval = cut_interval(mean_value, n=11))
@@ -45,20 +48,7 @@ vic_maps_et <- vic_maps_tib %>%
                                                         "350-400","400-450","450-500",
                                                          "500-550","550-600","600-700","700-800")))
 
-vic_maps_swe <- vic_maps_tib %>% 
-  dplyr::filter(flux_type=="swe") %>% 
-  #mutate(value_interval = cut_interval(mean_value, n=11))
-  dplyr::mutate(value_interval = cut_interval(mean_value,n=10,
-                                              breaks = c(0,200,400,600,
-                                                         800,1000,1200,
-                                                         1400,1600,1800,2000),
-                                              labels = c("0-200","200-400","400-600","600-800",
-                                                         "800-1000","1000-1200","1200-1400",
-                                                         "1400-1600","1600-1800","1800-2000")))
-
-# ----
-# Plot ET
-
+# Plot ET map
 vic_map <- function(tib, border, flux_type, flux_name, gcm_input){
   
   # Create labels for facetting
@@ -105,12 +95,70 @@ vic_map(tib=vic_maps_et, border=ss_border, flux_type = "et", flux_name = "Mean A
 vic_map(tib=vic_maps_et, border=ss_border, flux_type = "et", flux_name = "Mean Annual Evapotranspiration", gcm_input="hadgemec") 
 vic_map(tib=vic_maps_et, border=ss_border, flux_type = "et", flux_name = "Mean Annual Evapotranspiration", gcm_input="miroc5") 
 
-vic_map(tib=vic_maps_swe, border=ss_border, flux_type = "swe", flux_name = "Snow Water Equivalent", gcm_input="canesm2") 
-vic_map(tib=vic_maps_swe, border=ss_border, flux_type = "swe", flux_name = "Snow Water Equivalent", gcm_input="ccsm4") 
-vic_map(tib=vic_maps_swe, border=ss_border, flux_type = "swe", flux_name = "Snow Water Equivalent", gcm_input="cnrm") 
-vic_map(tib=vic_maps_swe, border=ss_border, flux_type = "swe", flux_name = "Snow Water Equivalent", gcm_input="hadgemcc") 
-vic_map(tib=vic_maps_swe, border=ss_border, flux_type = "swe", flux_name = "Snow Water Equivalent", gcm_input="hadgemec") 
-vic_map(tib=vic_maps_swe, border=ss_border, flux_type = "swe", flux_name = "Snow Water Equivalent", gcm_input="miroc5") 
+
+
+# ----
+# SWE Processing
+
+vic_maps_swe <- vic_maps_tib %>% 
+  dplyr::filter(flux_type=="swe") %>% 
+  #mutate(value_interval = cut_interval(mean_value, n=11))
+  dplyr::mutate(value_interval = cut_interval(mean_value,n=10,
+                                              breaks = c(0,200,400,600,
+                                                         800,1000,1200,
+                                                         1400,1600,2000),
+                                              labels = c("0-200","200-400","400-600","600-800",
+                                                         "800-1000","1000-1200","1200-1400",
+                                                         "1400-1600","1600-2000")))
+
+
+# Plot SWE map
+vic_map <- function(tib, border, flux_type, flux_name, gcm_input){
+  
+  # Create labels for facetting
+  period_id <- c(
+    `hist` = "Historical",
+    `45` = "End of Century (RCP4.5)",
+    `85` = "End of Century (RCP8.5)"
+  )
+  
+  tib_gcm <- dplyr::filter(tib, gcm == gcm_input)
+  
+  x <- ggplot(tib_gcm) +
+    geom_raster(aes(x=long,y=lat, fill=value_interval)) +
+    geom_sf(data=border, fill=NA, col="black") +
+    scale_fill_brewer(palette = "Blues", direction = 1, name="Mean Annual\nPeak SWE (mm)") +
+    scale_x_continuous(expand=c(0,0), limits = c(-119.8, -117.86)) +   # This eliminates margin buffer around plot
+    scale_y_continuous(expand=c(0,0), limits = c(35.65, 37.85)) +   # This eliminates margin buffer around plot
+    labs(title=paste(flux_name), x="Longitude",y="Latitude", size=0.5) +
+    theme_classic(base_size =10) +
+    theme(axis.text.x = element_text(angle = 330, hjust=0)) +
+    # geom_point(data = dplyr::filter(cities, name != 'Fresno'), aes(x = lon, y = lat), 
+    #            shape = 19, color = "black", fill = "grey50", size = 1.2) +
+    # geom_text(data = dplyr::filter(cities, name == 'Visalia'),
+    #           aes(x = lon, y = lat, label = paste("  ", as.character(name), sep="")),
+    #           size=3, angle = 0, vjust= -0.85, hjust = 0.95, color = "black") +
+    # geom_text(data = dplyr::filter(cities, name == 'Porterville'),
+    #           aes(x = lon, y = lat, label = paste("  ", as.character(name), sep="")),
+    #           size=3, angle = 0, vjust= -0.85, hjust = 1.1, color = "black") +
+    # geom_text(data = dplyr::filter(cities, name == 'Bishop'),
+    #           aes(x = lon, y = lat, label = paste("  ", as.character(name), sep="")),
+    #           size=3, angle = 0, vjust= -0.85, hjust = 0.95, color = "black") +
+  facet_grid(.~period, labeller = as_labeller(c(period_id))) +
+    theme(legend.position = "bottom") +
+    NULL
+  #plot(x)
+  
+  ggsave(paste("output/map_",flux_type,"_",gcm_input,".jpg",sep=""),plot=x, width = 7.2, height = 5)
+}
+
+
+vic_map(tib=vic_maps_swe, border=ss_border, flux_type = "swe", flux_name = "Mean Annual Peak Snow Water Equivalent", gcm_input="canesm2") 
+vic_map(tib=vic_maps_swe, border=ss_border, flux_type = "swe", flux_name = "Mean Annual Peak Snow Water Equivalent", gcm_input="ccsm4") 
+vic_map(tib=vic_maps_swe, border=ss_border, flux_type = "swe", flux_name = "Mean Annual Peak Snow Water Equivalent", gcm_input="cnrm") 
+vic_map(tib=vic_maps_swe, border=ss_border, flux_type = "swe", flux_name = "Mean Annual Peak Snow Water Equivalent", gcm_input="hadgemcc") 
+vic_map(tib=vic_maps_swe, border=ss_border, flux_type = "swe", flux_name = "Mean Annual Peak Snow Water Equivalent", gcm_input="hadgemec") 
+vic_map(tib=vic_maps_swe, border=ss_border, flux_type = "swe", flux_name = "Mean Annual Peak Snow Water Equivalent", gcm_input="miroc5") 
 
 
 
